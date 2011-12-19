85b9472cde07583460ba03cd551a9808e61ad478
Adrienne L. Travis <adrienne@utilitarienne.com>
2011-12-19 15:41:54 -0600

    Updated

--- system/expressionengine/third_party/alt_gistembed/pi.alt_gistembed.php
+++ system/expressionengine/third_party/alt_gistembed/pi.alt_gistembed.php
@@ -39,7 +39,8 @@ require_once PATH_THIRD .'alt_gistembed/MarkdownExtended/markdown_extended.php';
 class Alt_gistembed {
 
 	public $return_data;
-    public $url = 'https://raw.github.com/gist/%s';
+    public $rawurl = 'https://raw.github.com/gist/%s';
+    public $jsonurl = 'https://gist.github.com/%s.json';
     public $params = array();
     
 	/**
@@ -47,38 +48,61 @@ class Alt_gistembed {
 	 */
 	public function __construct() {
 		$this->EE =& get_instance();
+        $config = $this->EE->config;
+        
         $this->params['id'] = $this->_param('id', FALSE, FALSE, TRUE);
-        $this->params['markdown'] = $this->_param('markdown', TRUE, TRUE);
+        $this->params['markdown'] = $this->_param('markdown', FALSE, TRUE);
         $this->params['showtitle'] = $this->_param('showtitle', TRUE, TRUE);
         $this->params['showcite'] = $this->_param('showcite', TRUE, TRUE);
-        $this->return_data = $this->gist();
+
         
         } // end public function __construct()
     
+
     public function gist() {
-        $embedcall = file_get_contents(sprintf($this->url, $this->params['id'])); // Don't be afraid of sprintf!
-        $container = "<div class=\"gist-contents\">\n\n%s %s\n</div><!-- end .gist-contents -->";
-        $citation = '';
-  
-        if(FALSE == $this->params['showtitle']) {
-            $embedcall = strstr($embedcall,"\n");
-            }
+        $embedcall = file_get_contents(sprintf($this->rawurl,  $this->params['id'])); // Don't be afraid of sprintf!
+        $metadata  = file_get_contents(sprintf($this->jsonurl, $this->params['id'])); 
+        $metadata = json_decode($metadata,TRUE);
         
+        var_dump($metadata);
+        
+        $giststring = '';
+       
         if(TRUE == $this->params['markdown']) {
-            $embedcall = MarkdownExtended($embedcall);
+            $giststring = "<div id=\"gist-".$this->params['id']."\" class=\"gist\">\n<div class=\"gist-file\">\n";
+            $giststring .= "<div class=\"gist-data\">\n<div class=\"highlight gist-markdown\">".MarkdownExtended($embedcall)."\n</div>\n</div>";
             }
-            
+        else {
+            $giststring = $metadata['div'];
+            $giststring = substr($giststring,0,strpos($giststring,'<div class="gist-meta">'));
+            }
+
+
+        $citation = '';
+
         if(TRUE == $this->params['showcite']) {
-            $href = '<a href="https://gist.github.com/'.$this->params['id'].'">see original gist on Github.com</a>';
-            $citation = "\n\n<div class=\"gist-info\">$href</div>\n";
+            $href = '<a href="https://gist.github.com/'.$this->params['id'].'">This Gist</a>';
+            $owner = ' is owned by <a href="https://github.com/'.$metadata['owner'].'">'.$metadata['owner'].'</a> and ';
+            $citation = "\n\n<div class=\"gist-meta\">$href $owner brought to you by <a href=\"http://github.com\">GitHub.com</a></div>\n";
             }
         
-        $returnstring = sprintf($container,$embedcall,$citation);
+        $returnstring = $giststring.$citation."\n</div>\n</div>";
         
         return $returnstring;
        
-        } // end public function embed()
+        } // end public function gist()
     
+    
+    public function csslink() {
+        $returncss = '<style type="text/css">'."\n";
+        $returncss .= '@import url(https://gist.github.com/stylesheets/gist/embed.css);'."\n";
+        $returncss .= '.gist .gist-markdown { padding: 0 10px; font-family: sans-serif; font-size: 14px; }'."\n";
+        $returncss .= '.gist .gist-markdown code { font-size: 100%; font-family: Consolas, "DejaVu Mono", monospace; }'."\n";
+        $returncss .= '.gist .gist-syntax pre, .gist .gist-syntax pre div { font-size: 14px; font-family: Consolas, "DejaVu Mono", monospace; }'."\n";
+        $returncss .= '</style>'."\n";
+        
+        return $returncss;
+        } // end public function csslink()
     
     /* Thanks to ObjectiveHTML for this function! see https://gist.github.com/1478635 for details! */
     private function _param($param, $default = FALSE, $boolean = FALSE, $required = FALSE) {
@@ -112,16 +136,23 @@ class Alt_gistembed {
 		ob_start();
 ?>
 
-Usage: {exp:alt_gistembed id="xxxxxx"} where id is the ID # of a gist from GitHub.
+Main plugin usage: {exp:alt_gistembed:gist id="xxxxxx"} where id is the ID # of a gist from GitHub.
 
 Optional Parameters:
-showtitle (defaults to true): If set to false, strips the first line of the gist (presumably an H1).
 showcite (defaults to true): Shows a "see original gist on GitHub" link.
 markdown (defaults to true): Whether to run the returned gist through a Markdown parser.
 
 Note that the Markdown parser used is Markdown Extended Extra, https://github.com/egil/php-markdown-extra-extended .
-It is a fork of Markdown Extended by Michel Fortin which adds some extra features; among other things it is pretty close 
+It is a fork of Markdown Extended by Michel Fortin which adds some extra features. Its output is pretty close
 (but NOT identical) to GitHub-Flavored Markdown.
+
+You probably also want to embed the CSS in the head of the document; you can do that with the plugin as well.
+
+CSS usage: {exp:alt_gistembed:csslink}
+
+Optional Parameters:
+theme (defaults to github): A few different themes will eventually be available.
+
 <?php
 		$buffer = ob_get_contents();
 		ob_end_clean();
